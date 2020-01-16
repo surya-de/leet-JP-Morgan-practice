b25255b1b70967c2a4496d09da3f93d5eb52f77c
surya-de <schatt37@asu.edu>
2020-01-16 14:41:49 -0700

    selling time of a stock

---
+++ sell_stock_max_profit.py
@@ -0,0 +1,30 @@
+'''
+ First task would br to find
+ minimum pricr and followed
+ by that we need to find the
+ maximum profit associated to
+ that minimum value.
+'''
+import sys
+class Solution:
+    def maxProfit(self, prices):
+    	min_item = sys.maxsize
+    	max_profit = 0
+    	for elems in ls:
+    		# If price is less than
+    		# the minimum item till
+    		# now update the min val.
+    		if elems < min_item:
+    			min_item = elems
+    		# If the current list item
+    		# is not a minimum value
+    		# update the maximum profit
+    		# value associated with the
+    		# local minimum.
+    		elif elems - min_item > max_profit:
+    			max_profit = elems - min_item
+    	return max_profit
+if __name__ == '__main__':
+	ls = [7, 1, 5, 3, 6, 4]
+	s = Solution()
+	print(s.maxProfit(ls))
